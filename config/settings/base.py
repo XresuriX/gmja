bca63515b8a53e7dfa46ca27f5eff4ac0bb77dbd
leslie <taffe.leslie@gmail.com>
2025-05-15 19:01:26 -0400

    retry fix

--- config/settings/base.py
+++ config/settings/base.py
@@ -1,10 +1,11 @@
 # ruff: noqa: ERA001, E501
 """Base settings to build other settings files upon."""
 
-import ssl  # noqa: I001
+import ssl
 from pathlib import Path
 
 import environ
+
 from oscar.defaults import *  # noqa: F403
 
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent.parent
